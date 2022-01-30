7adf87aaf24bd84cf8b6a69012338497773d58ee
gantoin <gantoin@pm.me>
2022-01-30 20:33:18 +0100

    Create script for getting installed mods

---
+++ dl-mods.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl https://www.curseforge.com/minecraft/mc-mods/corpse/download/3625679/file -o corpse.jar
+curl https://www.curseforge.com/minecraft/mc-mods/dynamic-lights/download/3556319/file -o dynamic-lights.jar
+curl https://www.curseforge.com/minecraft/mc-mods/inventory-sorter/download/3543137/file -o inventory-sorter.jar
+curl https://www.curseforge.com/minecraft/mc-mods/jade/download/3614180/file -o jade.jar
+curl https://www.curseforge.com/minecraft/mc-mods/jei/download/3599262/file -o jei.jar
